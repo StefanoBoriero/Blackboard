16a1284d96db3f8d0d65787333b25f03fc3c87de
Stefano Boriero <stefano.boriero@gmail.com>
2017-11-22 18:38:23 +0100

    Added button waiting for context menu

---
+++ app/src/main/java/dima/it/polimi/blackboard/GroupListActivity.java
@@ -0,0 +1,19 @@
+package dima.it.polimi.blackboard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Stefano on 22/11/2017.
+ */
+
+public class GroupListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
--- app/src/main/java/dima/it/polimi/blackboard/MainActivity.java
+++ app/src/main/java/dima/it/polimi/blackboard/MainActivity.java
@@ -1,7 +1,10 @@
 package dima.it.polimi.blackboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void launchGroupList(View view){
+        Intent intent = new Intent(MainActivity.this, GroupListActivity.class);
+        startActivity(intent);
     }
 }
