8220326837b20559ee4a38568af6c9aa7d0802df
Simone Brunitti <Simone.brunitti@gmail.com>
2018-03-09 19:54:10 +0100

    Started creating the input details page

---
+++ app/src/main/java/dima/it/polimi/blackboard/activities/InsertDetailsActivity.java
@@ -0,0 +1,15 @@
+package dima.it.polimi.blackboard.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dima.it.polimi.blackboard.R;
+
+public class InsertDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_details);
+    }
+}
