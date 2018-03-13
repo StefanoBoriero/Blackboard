860e0a6b7a991deb598e6f48ea8b2d90a1c1a972
Simone Brunitti <Simone.brunitti@gmail.com>
2018-03-13 01:03:15 +0100

    Bills can now be created

--- app/src/main/java/dima/it/polimi/blackboard/activities/NewToDoTaskActivity.java
+++ app/src/main/java/dima/it/polimi/blackboard/activities/NewToDoTaskActivity.java
@@ -296,14 +296,14 @@ public class NewToDoTaskActivity extends AppCompatActivity {
         EditText currencyEditText = findViewById(R.id.costEditText);
         Spinner spinner = findViewById(R.id.spinner2);
         EditText description = findViewById(R.id.descriptionEditText);
-        //String amountString = currencyEditText.getText().toString().replace(",",".");
-        //Float amount = Float.parseFloat(amountString);
+        String amountString = currencyEditText.getText().toString().replace(",",".");
+        Float amount = Float.parseFloat(amountString);
         Map<String, Object> additionalInfo = new HashMap<>() ;
         additionalInfo.put("description", description.getText().toString());
-        //if(amount != 0.00)
-        //{
-            //additionalInfo.put("amount", amount);
-        //}
+        if(amount != 0.00)
+        {
+            additionalInfo.put("amount", amount);
+        }
 
 
 
