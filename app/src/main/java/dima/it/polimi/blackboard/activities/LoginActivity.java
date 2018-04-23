65ff71751e788ac726c640a03b4aebf6a922cab9
Simone Brunitti <Simone.brunitti@gmail.com>
2018-04-23 21:34:58 +0200

    Fixed small bug

--- app/src/main/java/dima/it/polimi/blackboard/activities/LoginActivity.java
+++ app/src/main/java/dima/it/polimi/blackboard/activities/LoginActivity.java
@@ -227,7 +227,7 @@ public class LoginActivity extends AppCompatActivity {
                     catch(FirebaseAuthInvalidCredentialsException e) {
                         Toast.makeText(LoginActivity.this,"Invalid username or password",Toast.LENGTH_SHORT).show();
                     }catch(Exception e) {
-                        Toast.makeText(LoginActivity.this,"Generic error",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginActivity.this,"Invalid username or password",Toast.LENGTH_SHORT).show();
                     }
 
                 }
